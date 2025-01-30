d1c3d714dfc120a06421681fe89a386d9460b93c
Yasmine Rantala <ycbran@utu.fi>
2025-01-30 23:19:40 +0200

    Exercise 1.8 unicafe step3

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,5 +1,19 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+    return (
+        <div>
+            <h2>Tilastoja</h2>
+            <p>Hyvä: {props.good}</p>
+            <p>Neutraali: {props.neutral}</p>
+            <p>Huono: {props.bad}</p>
+            <p>Yhteensä: {props.good+props.neutral+props.bad}</p>
+            <p>Keskiarvo: {props.good-props.bad/(props.good+props.neutral+props.bad)}</p>
+            <p>Positiivisia: {props.good/(props.good+props.neutral+props.bad)}</p>
+        </div>
+    )
+}
+
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
@@ -11,14 +25,8 @@ const App = () => {
             <button onClick={() => setGood(good+1)}>Hyvä</button>
             <button onClick={() => setNeutral(neutral+1)}>Neutraali</button>
             <button onClick={() => setBad(bad+1)}>Huono</button>
+            <Statistics good={good} neutral={neutral} bad={bad} />
 
-            <h2>Tilastoja</h2>
-            <p>Hyvä: {good}</p>
-            <p>Neutraali: {neutral}</p>
-            <p>Huono: {bad}</p>
-            <p>Yhteensä: {good+neutral+bad}</p>
-            <p>Keskiarvo: {good-bad/(good+neutral+bad)}</p>
-            <p>Positiivisia: {good/(good+neutral+bad)}</p>
         </div>
     )
 }
