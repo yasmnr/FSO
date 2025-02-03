380f7ae88b729a7479ea99d7af12b724e6ae38da
Yasmine Rantala <ycbran@utu.fi>
2025-02-03 22:56:11 +0200

    exercise 2.3 2.4 kurssitiedot step9

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,29 +1,54 @@
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        id: 1,
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3
-            }
-        ]
-    }
-
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
     return (
         <div>
-            <Course course={course} />
+            <h1>Web development curriculum</h1>
+            {courses.map(course =>
+                <Course key={course.id} course={course} />
+            )}
         </div>
     )
 }
@@ -41,7 +66,7 @@ const Course = (props) => {
 const Header = (props) => {
     return (
         <div>
-            <h1>{props.course}</h1>
+            <h2>{props.course}</h2>
         </div>
     )
 }
@@ -55,7 +80,7 @@ const Content = (props) => {
     )
 }
 const Total = (props) => {
-    const total = props.parts.reduce((sum, part) => sum + part.exercises, 0);
+    const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
     return (
         <div>
             <p><strong>total of {total} exercises</strong></p>
