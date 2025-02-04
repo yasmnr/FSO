4525630b70b3ab2aaeb749ad8ab5473087fd4750
Yasmine Rantala <ycbran@utu.fi>
2025-02-04 22:02:38 +0200

    exercise 2.6 puhelinluettelo step1

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ])
+    const [newName, setNewName] = useState('')
+
+    const addContact = (event) => {
+        event.preventDefault()
+        const contact = {name:newName}
+        setPersons(persons.concat(contact))
+        setNewName('')
+    }
+
+    const handleContactChange = (event) => {
+        setNewName(event.target.value)
+    }
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addContact}>
+                <div>
+                    name: <input value={newName} onChange={handleContactChange} />
+                    <br></br>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <ul>
+                {persons.map(person => (
+                    <li key={person.id}>{person.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+
+}
+export default App
