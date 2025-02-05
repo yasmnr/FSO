360ee0a8435e23aa72d698b8f97d58deb3c2f9e5
Yasmine Rantala <ycbran@utu.fi>
2025-02-05 19:57:59 +0200

    exercise 2.11 puhelinluettelo step6

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const ContactForm = (props) => {
     return (
@@ -35,15 +36,18 @@ const SearchBar = (props) => {
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Squid Game', number: '86504006' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [searchTerm, setSearchTerm] = useState('')
+
+    useEffect(()=>{
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons (response.data)
+            })
+    }, [])
 
     const addContact = (event) => {
         event.preventDefault()
